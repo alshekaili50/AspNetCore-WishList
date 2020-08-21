7b03532a5f23b07ada0c5c1f40ed7bd0632eb642
ali <alshekaili50@gmail.com>
2020-08-21 23:25:10 +0400

    hh

--- WishList/Data/ApplicationDbContext.cs
+++ WishList/Data/ApplicationDbContext.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using WishList.Models;
 
 namespace WishList.Data
 
@@ -10,7 +11,7 @@ namespace WishList.Data
 {
     public class ApplicationDbContext :DbContext
     {
-        public DbSet<Models.Item> Items;
+        public DbSet<Item> Items;
 
 
         public ApplicationDbContext(DbContextOptions options):base(options)
